5111fa8f74cd015debd814ca5baf2f0ed7ff34e0
felipreis <felipreisdaniel@gmail.com>
2024-06-26 18:43:16 -0300

    adicionando controller e service e tratemento de exceções

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package me.dio.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import me.dio.domain.model.User;
 
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByAccountNumber(String accountNumber);
 }
